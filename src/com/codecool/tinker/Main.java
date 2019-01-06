d244c86ef1926e30ee53cbe019d2bb9460d7ddbd
Aron Gida <ifAronisGida@github.com>
2019-01-06 12:44:19 +0100

    Initial commit

--- src/com/codecool/tinker/Main.java
+++ src/com/codecool/tinker/Main.java
@@ -1,8 +1,39 @@
 package com.codecool.tinker;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Number of players: ");
+        Scanner scanner = new Scanner(System.in);
+        int numberOfPlayers = scanner.nextInt();
+        System.out.println("Number of syllables: ");
+        int numberOfSyllables = scanner.nextInt();
+        scanner.close();
+        System.out.println("numberOfPlayers: " + numberOfPlayers);
+        System.out.println("numberOfSyllables: " + numberOfSyllables);
+
+        ArrayList<Integer> players = new ArrayList<>();
+        for (int i = 0; i < numberOfPlayers; i++) {
+            players.add(i+1);
+        }
+
+        Iterator<Integer> iterator = players.iterator();
+
+        int counter = 0;
+        while (players.size() > 1) {
+            if (!iterator.hasNext()) iterator = players.iterator();
+            iterator.next();
+            counter++;
+            if (counter == numberOfSyllables) {
+                iterator.remove();
+                counter = 0;
+            }
+        }
+
+        System.out.println("The winner is: " + players.get(0));
+
     }
 }
